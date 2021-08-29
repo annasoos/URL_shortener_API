97dbbb1293abdb35f77d2204751073e8db461342
Annamária Soós <annasoos94@gmail.com>
2021-08-29 20:11:26 +0200

    add shortener function, add linklist to session storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,19 @@ import Statistics from "./components/Statistics";
 import Boost from "./components/Boost";
 import Footer from "./components/Footer";
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 	return (
 		<>
+			<ToastContainer/>
 			<Header />
 			<Hero />
 			<Shortener />
 			<Statistics />
 			<Boost />
-			<Footer/>
+			<Footer />
 		</>
 	);
 }
--- frontend/src/components/Shortener.jsx
+++ frontend/src/components/Shortener.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
-import shortenerBG from "../images/bg-shorten-desktop.svg"
+import shortenerBG from "../images/bg-shorten-desktop.svg";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Shortener = () => {
+	const [longLink, setLongLink] = useState("");
+	const [linkList, setLinkList] = useState([]);
+
+	useEffect(() => {
+		sessionStorage.setItem("linkList", JSON.stringify(linkList));
+	}, [linkList]);
+
+	async function shortenLink(link) {
+
+		const body = { "long_url": longLink, "domain": "bit.ly" };
+		const headers = {
+			'Authorization': 'Bearer d089090cf1f3626904ca7c990d69b89b723a841b',
+			'Content-Type': 'application/json'
+		}
+		try {
+			const response = await axios.post(`https://api-ssl.bitly.com/v4/shorten`, body, { headers: headers })
+			let res = await response;
+			return setLinkList((oldList) => [...oldList, {original: longLink, shortened: res.data.link}])
+		}
+		catch (e) {
+			toast.error('Oops..something went wrong. Please try again!', { position: toast.POSITION.BOTTOM_CENTER, theme: "colored" });
+		}
+	}
+
+	console.log(linkList)
+
 	return (
-		<ShortenerContainer>
-			<ShortenerContent>
-				<Input type="url" name="url" id="url" autoComplete="off" placeholder="Shorten a link here..." pattern="https://.*" required></Input>
-				<Button>Shorten it!</Button>
-			</ShortenerContent>
-		</ShortenerContainer>
+		<>
+			<ShortenerContainer>
+				<ShortenerContent>
+					<Input type="url" name="url" id="url" autoComplete="off" placeholder="Shorten a link here..." pattern="https://.*" required onChange={(e) => setLongLink(e.target.value)}></Input>
+					<Button onClick={(e) => shortenLink(longLink)}> Shorten it! </Button>
+				</ShortenerContent>
+			</ShortenerContainer>
+
+			{/* {linkList.map((link, index) => (
+				<div>{link}</div>
+			))} */}
+		</>
 	)
 }
 
