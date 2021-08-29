d7ff9c90bc1476d09482717e016c6f01ffd41c94
Annamária Soós <annasoos94@gmail.com>
2021-08-29 21:48:47 +0200

    add copy button

--- frontend/src/components/LinkList.jsx
+++ frontend/src/components/LinkList.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from "styled-components";
 
 const LinkList = ({data, index}) => {
+
 	return (
 		<LinkListContainer>
 			<ListItem key={index}>
 				<span>{data.original}</span>
 				<span className="shortLink">{data.short}</span>
+				<Button> Copy </Button>
 			</ListItem>
 		</LinkListContainer>
 	)
@@ -21,12 +23,13 @@ const LinkListContainer = styled.section`
 `
 
 const ListItem = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
+	display: grid;
+	grid-template-columns: 5fr 1fr 1fr;
+	justify-content: start;
 	align-items: center;
 	gap: 2rem;
 
+	width: 100%;
 	padding: 0.8rem 1.5rem;
 	margin: 0.5rem 0;
 	
@@ -40,5 +43,24 @@ const ListItem = styled.div`
 		color: hsl(180, 66%, 49%);
 	}
 `
+
+const Button = styled.button`
+	font-size: 0.9rem;
+	font-weight: 700;
+	height: 2rem;
+	width: 8rem;
+
+  color: white;
+	background-color: hsl(180, 66%, 49%);
+	border-radius: 10px;
+
+  &:hover{
+		background-color: hsla(180, 66%, 49%, 0.5);
+	}
+
+	@media only screen and (max-width: 1090px) {
+		width: 100%;
+	};
+`;
 
 export default LinkList
